de6383d3eb7e957ea16ad5dfb102891b473802ca
atim <tamerlan.abilov95@gmail.com>
2018-12-08 00:17:34 +0400

    bot refactor API

---
+++ bot/core.py
@@ -0,0 +1,207 @@
+import asyncio
+import logging
+
+import aiohttp
+import random
+import string
+
+
+logger = logging.getLogger(__name__)
+
+
+class User:
+    """
+     Client for simulating user behaviour handling server API.
+     Username and password is necessary for refreshing token.
+    """
+
+    API_HOST = 'http://localhost/v1.0/'
+    SIGNUP_URL = API_HOST + 'signup/'
+    LOGIN_URL = API_HOST + 'token/'
+    POST_URL = API_HOST + 'posts/'
+
+    LIKE_URL = API_HOST + 'posts/{id}/like/'
+
+    CONTENT = """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+                 eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                 nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                 reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+                 officia deserunt mollit anim id est laborum."""
+
+    _total_posts = []
+
+    def __init__(self, session, username, password, max_posts=5, max_likes=5, token=None):
+        """
+
+        """
+
+        self.username = username
+        self.password = password
+        self.session = session
+        self.max_posts = max_posts
+        self.max_likes = max_likes
+        self.posts, self.likes = 0, 0
+        self.token = token
+
+    @staticmethod
+    def __generate_string(length=8):
+        return ''.join(
+            random.choices(string.ascii_uppercase + string.digits, k=length)
+        )
+
+    @classmethod
+    async def generate(cls, session, **kwargs):
+        """
+        Generate's new random user.
+        :param session:
+        :param kwargs: All other user parameters
+        :return:
+        """
+        user = User(
+            session,
+            username=User.__generate_string(),
+            password=User.__generate_string(length=20),
+            **kwargs
+        )
+
+        # return asyncio.ensure_future(user.signup())
+        return await user.signup()
+
+    def __exhausted_limit(self, attr):
+
+        if getattr(self, attr) >= getattr(self, 'max_' + attr):
+            logger.warning(f'Number of {attr} for user {self.username} exhausted')
+            return True
+
+        return False
+
+    async def _request(self, method, url, *args, **kwargs):
+
+        exc = None
+        if self.token:
+            kwargs.setdefault('headers', {}).update(self._auth_header())
+
+        # server drops connection so we use 20 attempts to not miss any req.
+        for i in range(20):
+            try:
+                async with self.session.request(method, url, *args, **kwargs) as response:
+
+                    # TODO: check if status 401 refresh token
+                    return await response.json(), response.status
+
+            except aiohttp.ClientError as e:
+                exc = e
+                # print(f'{i} Server disconnect... Try again {url}')
+        logger.error(f'Host error after {i} times: {exc}')
+        raise Exception(f'Host error after {i} times: {exc}')
+
+    def _auth_header(self):
+
+        return {
+            "Authorization": "Bearer %s" % self.token
+        }
+
+    async def _get_token(self):
+
+        # we can load users from some file or external resources as example
+
+        response, status = await self._request('POST', User.LOGIN_URL,
+                                               data={
+                                                    "username": self.username,
+                                                    "password": self.password
+                                               })
+        if status != 200:
+            logger.warning('Error ', response)
+            raise Exception('Invalid token')
+        self.token = response['access']
+
+    @property
+    def left_likes(self):
+        return self.max_likes - self.likes
+
+    @property
+    def left_posts(self):
+        return self.max_posts - self.posts
+
+    async def create_content(self, content=CONTENT):
+
+        if self.__exhausted_limit('posts'):
+            return
+
+        post, status = await self._request('POST', User.POST_URL,
+                                           data={"content": content})
+        if status == 200:
+            self.posts += 1
+        return post
+
+    async def like(self, post_id):
+        if self.__exhausted_limit('likes'):
+            return
+
+        message, status = await self._request(
+            'POST', User.LIKE_URL.format(id=post_id)
+        )
+
+        if status == 200:
+            self.likes += 1
+
+        return status
+
+    async def signup(self):
+        _, status = await self._request('POST', User.SIGNUP_URL,
+                                        data={
+                                            "username": self.username,
+                                            "password": self.password
+                                        })
+
+        if status != 200:
+            raise Exception('signup failed')
+
+        await self._get_token()
+        return self
+
+    async def generate_content(self):
+        """
+         Generate posts for amount of quota left
+        :return:
+        """
+        # generate content
+        if self.__exhausted_limit('posts'):
+            return
+
+        posts = await asyncio.gather(*[
+            self.create_content(f"{self.username} content #{i}: {User.CONTENT} ")
+            for i in range(self.left_posts)
+        ])
+
+        # save generated posts to not waste server resources.
+        User._total_posts.extend(map(lambda i: i['id'], posts))
+
+        return posts
+
+    async def generate_likes(self):
+        """
+
+        Generate likes for amount of quota left
+        :return:
+        """
+        if self.__exhausted_limit('likes'):
+            return
+
+        await asyncio.gather(*[
+
+            self.like(pid)
+            for pid in random.choices(User._total_posts, k=self.left_likes)
+        ])
+
+    async def simulate(self):
+        """
+        Let bot simulate fake behaviour with best scenario.
+        :return:
+        """
+
+        # TODO: Declarative way of simulation
+        await self.generate_content()
+        await self.generate_likes()
--- bot/social_bot.py
+++ bot/social_bot.py
@@ -1,28 +1,20 @@
 import argparse
 import json
-import random
+import logging
+
 import sys
 from json import JSONDecodeError
-from random import choice
 
 import aiohttp
 import asyncio
 
 import uvloop
 
-# uwsgi conf port used
-API_URL = 'http://localhost/v1.0/'
+from core import User
 
-generated_posts_id = []
+logging.basicConfig(stream=sys.stdout,level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
-
-CONTENT = """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
-             eiusmod tempor incididunt ut labore et dolore magna aliqua.
-             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-             nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
-             officia deserunt mollit anim id est laborum."""
 
 parser = argparse.ArgumentParser(description='Behaviour rules')
 
@@ -41,138 +33,60 @@ parser.add_argument('-f', '--config', type=str, dest='config-file',
                     help='Maximum number of random likes created by user',
                     required=False)
 
-args = vars(parser.parse_args())
-RULES = args
-# can read from args too.
-if args.get('config-file'):
+
+RULES = vars(parser.parse_args())
+
+
+def load_config(path):
     try:
-        with open(args.get('config-file')) as conf:
+        with open(path) as conf:
             try:
                 data = json.load(conf)
             except JSONDecodeError as e:
-                sys.exit(f'Error loading config.json: {e}')
+                sys.exit(f'Error loading {path}: {e}')
 
             if any(
                 type(v) is not int or v < 0 for v in data.values()
                 if v in RULES.keys()
             ):
                 sys.exit('ERROR: Values must be integer')
-
-            RULES.update(data)
-            print('Configuration loaded...')
+            logger.info('Configuration loaded...')
+            return data
 
     except FileNotFoundError:
-        print('Skip bad file  configuration... Using args as default.')
+        logger.warning('Skip bad file  configuration... Using args as default.')
 
 
-print(RULES)
+if RULES.get('config-file'):
+    RULES.update(load_config(RULES.get('config-file')))
 
 
-class User:
-    def __init__(self, bot_id, session):
-        self.username = f'verytrickybot{bot_id}'
-        self.password = 'mydumbpassword'
-        self.session = session
-        self.token = None
+logger.info(f'Rules:  {RULES}')
 
-    async def request(self, method, url, *args, **kwargs):
 
-        # server drops connection so we use 20 attempts to not miss any req.
-        exc = None
-        for i in range(20):
-            try:
-                async with self.session.request(method, url, *args, **kwargs) as response:
-                    return await response.json()
-
-            except aiohttp.ClientError as e:
-                exc = e
-                # print(f'{i} Server disconnect... Try again {url}')
-
-        sys.exit(f'Host error: {exc}')
-
-    async def _create_content(self, content):
-        return await self.request('POST', API_URL + 'posts/', headers={
-            "Authorization": f"Bearer {self.token}"
-        }, data={"content": content})
-
-    async def get_token(self):
-
-        # we can load users from some file or external resources as example
-
-        await self.request('POST', API_URL + 'signup/',
-                           data={"username": self.username,
-                                 "password": self.password})
-
-        response = await self.request('POST', API_URL + 'token/',
-                                      data={"username": self.username,
-                                            "password": self.password})
-        self.token = response['access']
-
-    async def process_content(self):
-
-        # set token and start generating content immediately
-        await self.get_token()
-
-        # generate content
-        posts = await asyncio.gather(*[
-            asyncio.ensure_future(self._create_content(f"{self.username} content #{i}: {CONTENT} "))
-            for i in range(0, random.randint(0, RULES['max_user_posts']))
-        ])
-
-        # save generated posts to not waste server resources.
-        generated_posts_id.extend(map(lambda i: i['id'], posts))
-        # print(f'{self.username} processed -> ', len(posts))
-
-        return posts
-
-    async def like_content(self):
-
-        def generate_random(times):
-            return map(lambda i: choice(generated_posts_id), range(times))
-
-        await asyncio.gather(*[
-            asyncio.ensure_future(
-                self.request(
-                    'POST', f'{API_URL}posts/{i}/like/',
-                    headers={
-                        "Authorization": f"Bearer {self.token}"
-                    }
-                ))
-            for i in generate_random(random.randint(0, RULES['max_user_likes']))
-        ])
-
-        # print(f'Finished  {self.username}' , result)
-
-    async def process(self):
-        await self.get_token()
-        await self.process_content()
-        await self.like_content()
+async def simulate_when_done(coro):
+    user = await coro
+    await user.simulate()
+    return user
 
 
 async def main():
 
     async with aiohttp.ClientSession(connector=aiohttp.TCPConnector()) as session:
-        users = [User(i, session) for i in range(RULES['users'])]
 
-        print('Generating content...')
+        logger.info('Start generating users...')
+        tasks = [asyncio.ensure_future(User.generate(session)) for _ in range(RULES['users'])]
+
+        logger.info('Waiting for generating content...')
+        # Start simulation right after release of each task
         await asyncio.gather(*[
-            asyncio.ensure_future(user.process_content())
-            for user in users
+            asyncio.ensure_future(simulate_when_done(awaited_user))
+            for awaited_user in tasks
         ])
 
-        print('Content generated... %s posts ' % len(generated_posts_id))
-
-        print('Start promoting content...')
-        await asyncio.gather(*[
-            asyncio.ensure_future(user.like_content())
-            for user in users
-        ])
-
-        print('Content randomly liked...')
-
+        logger.info('Simulation ended successfully...')
 
 if __name__ == '__main__':
-    # we can keep plain asyncio loop as well
     asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
